38a2cbe3f56be265ef8d561781086dfae9531998
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-02-03 14:45:25 +0200

    Implement a scrollable text log at bottom of UI
    Started from buggy pickUp-animation branch. Fixes here may differ.
    New attributes (mostly UI Node elements) were added to Main.
    A refactor into a UI package might be needed soon.

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -1,35 +1,40 @@
 package com.codecool.dungeoncrawl;
 
 import com.codecool.dungeoncrawl.logic.Cell;
-import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.GameMap;
 import com.codecool.dungeoncrawl.logic.MapLoader;
-import com.codecool.dungeoncrawl.logic.actors.Actor;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.KeyCode;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import javax.swing.plaf.basic.BasicArrowButton;
-
 public class    Main extends Application {
+    public static final Integer MAX_LOG_LENGTH = 1000;
     GameMap map = MapLoader.loadMap();
     Canvas canvas = new Canvas(
             map.getWidth() * Tiles.TILE_WIDTH,
             map.getHeight() * Tiles.TILE_WIDTH);
     GraphicsContext context = canvas.getGraphicsContext2D();
     Label healthLabel = new Label();
+    GridPane uiDashboard = new GridPane();
+
+    StringBuilder logStringBuilder = new StringBuilder(MAX_LOG_LENGTH / 10);
+
+
+    ScrollPane scrollForLogArea = new ScrollPane();
+    TextArea logArea = new TextArea();
 
     public static void main(String[] args) {
         launch(args);
@@ -37,41 +42,75 @@ public class    Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        GridPane ui = new GridPane();
-        ui.setPrefWidth(200);
-        ui.setPadding(new Insets(10));
-        ui.setVgap(10);
 
-        ui.add(new Label("Health: "), 0, 0);
-        ui.add(healthLabel, 1, 0);
+        pushInLog("Log me... \tRight about now\tThe funk soul brother!");
+        uiDashboard.setPrefWidth(200);
+        uiDashboard.setPadding(new Insets(10));
+        uiDashboard.setVgap(10);
+
+        uiDashboard.add(new Label("Health: "), 0, 0);
+        uiDashboard.add(healthLabel, 1, 0);
 
         Button pickUp = new Button("Pick Up");
         pickUp.setVisible(false);
 
-        ui.add(pickUp,0,1);
 
+        uiDashboard.add(pickUp,0,1);
+
+
+        GridPane uiBottomPane = new GridPane();
+
+
+        scrollForLogArea.setMinHeight(80);
+        logArea.setPrefWidth(canvas.getWidth());
+        scrollForLogArea.setContent(logArea);
+//        logArea.textProperty().bind(logTextObject.textProperty());
+//        logTextObject.wrappingWidthProperty().bind(scrollForLogArea.widthProperty());
+
+
+
+
+//        scrollForLog.setStyle("-fx-opacity: 1; "+
+//                "-fx-background-color: GREEN;");
+        logArea.setFocusTraversable(false);
+        logArea.setStyle("-fx-opacity: 1;" /* +
+                "-fx-background-color: RED;" */
+                );
+//        uiBottomPane.setStyle("-fx-background-color: BLUE;");
+        uiBottomPane.getChildren().add(scrollForLogArea);
 
         BorderPane borderPane = new BorderPane();
 
 
         borderPane.setCenter(canvas);
-        borderPane.setRight(ui);
+        borderPane.setBottom(uiBottomPane);
+        borderPane.setRight(uiDashboard);
 
         Scene scene = new Scene(borderPane);
         primaryStage.setScene(scene);
         refresh();
-        scene.setOnKeyPressed(this::onKeyPressed);
-        scene.setOnKeyReleased(keyEvent -> {
-            pickUp.visibleProperty().set(map.getPlayer().getCell().getItem() != null);
-        });
-
-
-
-
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, this::onKeyPressed);
 
         primaryStage.setTitle("Dungeon Crawl");
         primaryStage.show();
     }
+
+// Example of getting elements from parent Panes, Scene, etc (in this case uiDashboard)
+
+    private Button getPickUpButton() {
+        ObservableList<Node> nodeElementsList;
+        nodeElementsList = uiDashboard.getChildren();
+        for (Node element : nodeElementsList ) {
+            if (element instanceof Button) {
+                Button elementAsButton = (Button) element;
+                if (elementAsButton.getText() == "Pick Up") {
+                    return elementAsButton;
+                }
+            }
+        }
+        return null;
+    }
+
 
     private void onKeyPressed(KeyEvent keyEvent) {
         switch (keyEvent.getCode()) {
@@ -94,6 +133,33 @@ public class    Main extends Application {
         }
     }
 
+    public void pushInLog(String message)
+    {
+        Integer messageLength = message.length();
+        Integer currentLogLength = logStringBuilder.length();
+
+        logStringBuilder.append("\n" + message);
+        String newLogText;
+        Integer newLogLength = messageLength + currentLogLength;
+        if (newLogLength > MAX_LOG_LENGTH) {
+            newLogText = logStringBuilder
+                    .subSequence(Math.max(0,newLogLength-MAX_LOG_LENGTH), currentLogLength)
+                    .toString() ;
+            logStringBuilder.setLength(0);
+            logStringBuilder.append(newLogText.substring(MAX_LOG_LENGTH/2));
+        }
+        else {
+            newLogText = logStringBuilder.toString();
+        }
+        logArea.setFocusTraversable(true);
+
+
+        logArea.setText(newLogText);    // can bind this to a Text object
+        logArea.selectPositionCaret(logArea.getLength());
+        logArea.setFocusTraversable(false);
+        logArea.deselect();
+    }
+
     private void refresh() {
         context.setFill(Color.BLACK);
         context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -111,6 +177,10 @@ public class    Main extends Application {
                 }
             }
         }
+        if (getPickUpButton() != null) {
+            getPickUpButton().setVisible(map.getPlayer().getCell().getItem() != null);
+        }
+        pushInLog("Refresh happened!");
         healthLabel.setText("" + map.getPlayer().getHealth());
     }
 }
--- src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Item.java
@@ -15,5 +15,4 @@ public abstract class Item implements Drawable {
         return cell;
     }
 
-    public abstract String getTileName();
 }

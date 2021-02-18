0d55f510c2b13dedc92c3364a28ee06aee7c0d55
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-02-18 11:56:53 +0200

    Update schema_ddl

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS public.game_state;
+DROP TABLE IF EXISTS public.game_state CASCADE;
 CREATE TABLE public.game_state (
     id serial NOT NULL PRIMARY KEY,
     current_map text NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE public.game_state (
     player_id integer NOT NULL
 );
 
-DROP TABLE IF EXISTS public.player;
+DROP TABLE IF EXISTS public.player CASCADE;
 CREATE TABLE public.player (
     id serial NOT NULL PRIMARY KEY,
     player_name text NOT NULL,
@@ -27,7 +27,7 @@ CREATE TABLE public.gamestate_item (
                     item_id integer NOT NULL
 );
 
-DROP TABLE IF EXISTS public.actor
+DROP TABLE IF EXISTS public.actor CASCADE;
 CREATE TABLE public.actor (
       id serial NOT NULL PRIMARY KEY,
       class_name text NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE public.actor (
       default_id integer NOT NULL
 );
 
-DROP TABLE IF EXISTS public.default_actor
+DROP TABLE IF EXISTS public.default_actor;
 CREATE TABLE public.default_actor (
       id serial NOT NULL PRIMARY KEY,
       class_name text NOT NULL,
@@ -46,18 +46,48 @@ CREATE TABLE public.default_actor (
       y integer NOT NULL
 );
 
-DROP TABLE IF EXISTS public.item
+DROP TABLE IF EXISTS public.item CASCADE;
 CREATE TABLE public.item (
-                              id serial NOT NULL PRIMARY KEY,
-                              class_name text NOT NULL,
-                              is_in_inventory boolean NOT NULL,
-                              x integer,
-                              y integer,
-                              default_id integer NOT NULL
+      id serial NOT NULL PRIMARY KEY,
+      class_name text NOT NULL,
+      is_in_inventory boolean NOT NULL,
+      x integer,
+      y integer,
+      default_id integer NOT NULL
 );
 
-
-
+DROP TABLE IF EXISTS public.default_item;
+CREATE TABLE public.default_item  (
+    id serial NOT NULL PRIMARY KEY,
+    class_name text NOT NULL,
+    is_in_inventory boolean NOT NULL,
+    x integer,
+    y integer
+);
 
 ALTER TABLE ONLY public.game_state
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+
+ALTER TABLE ONLY public.gamestate_actor
+    ADD CONSTRAINT pk_gamestate_actor_id PRIMARY KEY (gamestate_id, actor_id);
+
+ALTER TABLE ONLY public.gamestate_actor
+    ADD CONSTRAINT fk_gamestate_id FOREIGN KEY (gamestate_id) REFERENCES public.game_state(id);
+
+ALTER TABLE ONLY public.gamestate_actor
+    ADD CONSTRAINT fk_actor_id FOREIGN KEY (actor_id) REFERENCES public.actor(id);
+
+ALTER TABLE ONLY public.gamestate_item
+    ADD CONSTRAINT pk_gamestate_item_id PRIMARY KEY (gamestate_id, item_id);
+
+ALTER TABLE ONLY public.gamestate_item
+    ADD CONSTRAINT fk_gamestate_id FOREIGN KEY (gamestate_id) REFERENCES public.game_state(id);
+
+ALTER TABLE ONLY public.gamestate_item
+    ADD CONSTRAINT fk_item_id FOREIGN KEY (item_id) REFERENCES public.item(id);
+
+ALTER TABLE ONLY public.actor
+    ADD CONSTRAINT fk_default_id FOREIGN KEY (default_id) REFERENCES public.default_actor(id);
+
+ALTER TABLE ONLY public.item
+    ADD CONSTRAINT fk_default_id FOREIGN KEY (default_id) REFERENCES public.default_item(id);

f00a3cbc35b144af939961b689a38f1ec7c5636e
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-02-19 09:19:21 +0200

    Modify GameState model to support Table display

--- src/main/java/com/codecool/dungeoncrawl/model/GameState.java
+++ src/main/java/com/codecool/dungeoncrawl/model/GameState.java
@@ -1,19 +1,31 @@
 package com.codecool.dungeoncrawl.model;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 import java.util.ArrayList;
 import java.sql.Date;
 import java.util.List;
 
 public class GameState extends BaseModel {
-    private Date savedAt;
+    private Date savedAt;   // java.sql.Date is just the Year-Month-Day (no hour:min)
     private String currentMap;
     private List<String> discoveredMaps = new ArrayList<>();
+    private List<ItemModel> itemDeltas = new ArrayList<>();
     private PlayerModel player;
+    private final SimpleStringProperty currentMapForTable;
+    private final SimpleStringProperty saveDateForTable;
+    private final SimpleStringProperty playerNameForTable;
+    private final SimpleIntegerProperty playerHpForTable;
 
     public GameState(String currentMap, Date savedAt, PlayerModel player) {
         this.currentMap = currentMap;
         this.savedAt = savedAt;
         this.player = player;
+        currentMapForTable = new SimpleStringProperty(currentMap);
+        saveDateForTable = new SimpleStringProperty(savedAt.toString());
+        playerNameForTable = new SimpleStringProperty(player.getPlayerName());
+        playerHpForTable = new SimpleIntegerProperty(player.getHp());
     }
 
     public Date getSavedAt() {
@@ -46,5 +58,45 @@ public class GameState extends BaseModel {
 
     public void setPlayer(PlayerModel player) {
         this.player = player;
+    }
+
+    public List<ItemModel> getItemDeltas() {
+        return itemDeltas;
+    }
+
+    public void addItemDelta(ItemModel item) {
+        this.itemDeltas.add(item);
+    }
+
+    public String getCurrentMapForTable() {
+        return currentMapForTable.get();
+    }
+
+    public SimpleStringProperty currentMapForTableProperty() {
+        return currentMapForTable;
+    }
+
+    public String getSaveDateForTable() {
+        return saveDateForTable.get();
+    }
+
+    public SimpleStringProperty saveDateForTableProperty() {
+        return saveDateForTable;
+    }
+
+    public String getPlayerNameForTable() {
+        return playerNameForTable.get();
+    }
+
+    public SimpleStringProperty playerNameForTableProperty() {
+        return playerNameForTable;
+    }
+
+    public int getPlayerHpForTable() {
+        return playerHpForTable.get();
+    }
+
+    public SimpleIntegerProperty playerHpForTableProperty() {
+        return playerHpForTable;
     }
 }
